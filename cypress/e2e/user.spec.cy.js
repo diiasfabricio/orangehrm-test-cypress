00a57cc5bf0f782414130566608dede53f3fa10d
Fabrício Dias <105397346+diiasfabricio@users.noreply.github.com>
2025-01-16 12:06:53 -0300

    Combobox tested

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -16,6 +16,7 @@ describe('Orange HRM Tests', () => {
     dateField: "[placeholder='dd-mm-yyyy']",
     dateCloseButton: ".--close",
     subimitButton: "[type='submit']",
+    genericComboBox: ".oxd-select-text--arrow"
 
   }
 
@@ -30,13 +31,27 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.dashboardGrid)
     cy.get(selectorsList.myInfoButton).click()
     cy.get(selectorsList.firstNameField).clear().type('FirstnameTest')
-    cy.get(selectorsList.genericField).eq(3).clear().type('EmpIdTest')
-    cy.get(selectorsList.genericField).eq(4).clear().type('otIdTest')
-    cy.get(selectorsList.genericField).eq(5).clear().type('dLNTest')
-    cy.get(selectorsList.genericField).eq(6).clear().type('2027-03-25')
+    cy.get(selectorsList.lastNameField).clear().type('LastnameTest')
+    cy.get(selectorsList.genericField).eq(3).clear().type('nNTest')
+    cy.get(selectorsList.genericField).eq(4).clear().type('EmpIdTest')
+    cy.get(selectorsList.genericField).eq(5).clear().type('otIdTest')
+    cy.get(selectorsList.genericField).eq(6).clear().type('dLNTest')
+    cy.get(selectorsList.genericField).eq(7).clear().type('2027-03-25')
     cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.genericField).eq(8).clear().type('tFieldTest')
-    cy.get(selectorsList.subimitButton).eq(1).click()
+    cy.get(selectorsList.genericField).eq(8).clear().type('ssnNTest')
+    cy.get(selectorsList.genericField).eq(9).clear().type('sinNTest')
+    // cy.get(selectorsList.genericField).eq(12).clear().type('militarySTest')
+    // cy.get(':nth-child(2) > .oxd-checkbox-wrapper > label').click()
+    // cy.get(selectorsList.genericField).eq(11).clear().type('tFieldTest')
+    cy.get(selectorsList.genericComboBox).eq(0).click({force: true})
+    cy.get('.oxd-select-dropdown > :nth-child(28)').click()
+    cy.get(selectorsList.genericComboBox).eq(1).click({force: true})
+    cy.get('.oxd-select-dropdown > :nth-child(3)').click()
+    cy.get(selectorsList.subimitButton).eq(0).click({force: true})
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get(selectorsList.genericComboBox).eq(2).click({force: true})
+    cy.get('.oxd-select-dropdown > :nth-child(6)').click()
+    cy.get(selectorsList.subimitButton).eq(1).click({force: true})
     cy.get('body').should('contain', 'Successfully Saved')
   })
   it('Login com fail', () => {
