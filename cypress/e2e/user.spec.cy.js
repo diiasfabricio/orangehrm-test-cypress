002bf649047627e64e8d448a182fcba087a3ed89
Fabrício Dias <105397346+diiasfabricio@users.noreply.github.com>
2025-01-17 17:11:27 -0300

    Add login, menu e my infor page

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -2,63 +2,36 @@ import userData from '../fixtures/user-data.json'
 import LoginPage from '../pages/loginPage'
 import DashBoardPage from '../pages/dashBoardPage'
 import MenuPage from '../pages/menuPage'
+import MyInfoPage from '../pages/myInfoPage'
 
 const loginPage = new LoginPage()
 const dashBoardPage =  new DashBoardPage()
 const menuPage = new MenuPage()
+const myInfoPage = new MyInfoPage()
 
 describe('Orange HRM Tests', () => {
 
-  const selectorsList = {
 
-    firstNameField: "[name='firstName']",
-    lastNameField: "[name='lastName']",
-    genericField: ".oxd-input--active",
-    dateField: "[placeholder='dd-mm-yyyy']",
-    genericComboBox: ".oxd-select-text--arrow",
-    secondItemCombox: ".oxd-select-dropdown > :nth-child(2)",
-    thirdItemCombox: ".oxd-select-dropdown > :nth-child(3)",
-    dateCloseButton: ".--close",
-    subimitButton: "[type='submit']"
-    
-  }
-
-  it.only('User infor Update - Success', () => {
+  it('User infor Update - Success', () => {
     loginPage.accessLoginPage()
     loginPage.loginAnyUser(userData.userSuccess.username, userData.userSuccess.password)
+
     dashBoardPage.checkDashboardPage()
+
     menuPage.accessMyInfo()
 
+    myInfoPage.fillPersonalDetails('First Name', 'Middle Name', 'Last Name')   
+    myInfoPage.fillEmployeeDetails('Employe Id', 'Other Id', 'Drive Licence Number', '2025-07-29', '123456', '987654')
+    myInfoPage.fillStatus()
+    myInfoPage.saveForm()
     
-    cy.get(selectorsList.firstNameField).clear().type('FirstnameTest')
-    cy.get(selectorsList.lastNameField).clear().type('LastnameTest')
-    cy.get(selectorsList.genericField).eq(3).clear().type('nNTest')
-    cy.get(selectorsList.genericField).eq(4).clear().type('EmpIdTest')
-    cy.get(selectorsList.genericField).eq(5).clear().type('otIdTest')
-    cy.get(selectorsList.genericField).eq(6).clear().type('dLNTest')
-    cy.get(selectorsList.genericField).eq(7).clear({force: true}).type('2027-03-25')
-    cy.get(selectorsList.dateCloseButton).click({force: true})
-    cy.get(selectorsList.genericField).eq(8).clear().type('ssnNTest')
-    //cy.get(selectorsList.genericField).eq(11).clear().type('sinNTest')
-    // cy.get(selectorsList.genericField).eq(12).clear().type('militarySTest')
-    // cy.get(':nth-child(2) > .oxd-checkbox-wrapper > label').click()
-    // cy.get(selectorsList.genericField).eq(11).clear().type('tFieldTest')
-    cy.get(selectorsList.genericComboBox).eq(0).click({force: true})
-    cy.get('.oxd-select-dropdown > :nth-child(28)').click({force: true})
-    cy.get(selectorsList.genericComboBox).eq(1).click({force: true})
-    cy.get('.oxd-select-dropdown > :nth-child(3)').click({force: true})
-    // cy.get(selectorsList.subimitButton).eq(0).click({force: true})
-    // cy.get('body').should('contain', 'Successfully Updated')
-    cy.get(selectorsList.genericComboBox).eq(2).click({force: true})
-    cy.get('.oxd-select-dropdown > :nth-child(6)').click({force: true})
-    cy.get(selectorsList.subimitButton).eq(1).click({force: true})
-    cy.get('body').should('contain', 'Successfully Saved')
+   
   })
-  it('Login com fail', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userFail.username)
-    cy.get(selectorsList.passwordField).type(userData.userFail.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.get(selectorsList.wrongCredentialAlert)
+
+  it('Login - fail', () => {
+    
+    loginPage.accessLoginPage()
+    loginPage.loginAnyUser(userData.userFail.username, userData.userFail.password)
+    loginPage.checkAccessIvalid()
   })
 })
---
+++ cypress/pages/myInfoPage.js
@@ -0,0 +1,53 @@
+class MyInfoPage {
+
+    selectorsList() {
+        const selectors = {
+            firstNameField: "[name='firstName']",
+            middleNameField: "[name='middleName']",
+            lastNameField: "[name='lastName']",
+            genericField: ".oxd-input--active",
+            dateField: "[placeholder='dd-mm-yyyy']",
+            genericComboBox: ".oxd-select-text--arrow",
+            secondItemCombobox: ".oxd-select-dropdown > :nth-child(2)",
+            thirdItemCombobox: ".oxd-select-dropdown > :nth-child(3)",
+            dateCloseButton: ".--close",
+            subimitButton: "[type='submit']",    
+        }   
+
+        return selectors
+
+    }
+
+    fillPersonalDetails(firstName, middleName, lastName) {
+        cy.get(this.selectorsList().firstNameField).clear().type(firstName)
+        cy.get(this.selectorsList().middleNameField).clear().type(middleName)
+        cy.get(this.selectorsList().lastNameField).clear().type(lastName)
+        
+    }
+
+    fillEmployeeDetails (employeeId, otherId, driverLicense, driversLicenseDate, ssnNuber, sinNuber) {
+        cy.get(this.selectorsList().genericField).eq(3).clear().type(employeeId)
+        cy.get(this.selectorsList().genericField).eq(4).clear().type(otherId)
+        cy.get(this.selectorsList().genericField).eq(5).clear().type(driverLicense)
+        cy.get(this.selectorsList().genericField).eq(6).clear().type(driversLicenseDate)
+        cy.get(this.selectorsList().dateCloseButton).click({force: true})
+        // cy.get(this.selectorsList().genericField).eq(8).clear().type(ssnNuber)
+        // cy.get(this.selectorsList().genericField).eq(9).clear().type(sinNuber)
+    }
+
+    saveForm() {
+        cy.get(this.selectorsList().subimitButton).eq(0).click({force: true})
+        cy.get('body').should('contain', 'Successfully Updated')
+        cy.get('.oxd-toast-close')
+    }
+
+    fillStatus(){
+        cy.get(this.selectorsList().genericComboBox).eq(0).click({force: true})
+        cy.get(this.selectorsList().secondItemCombobox).click({force: true})
+        cy.get(this.selectorsList().genericComboBox).eq(1).click({force: true})
+        cy.get(this.selectorsList().thirdItemCombobox).click({force: true})
+    }
+
+}
+
+export default MyInfoPage
